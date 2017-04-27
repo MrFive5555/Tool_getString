3e7d32f61fbac690955255351c8693ee0d7a0a4c
wzx <wzx@LAPTOP-6HP82F4G>
2017-04-27 22:27:07 +0800

    添加项目文件。

---
+++ password.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+#pragma warning (disable:4996)
+
+using namespace std;
+
+string getPassword()
+{
+    string retstr;
+    char ch = 0;
+    while ((ch = getch()) != '\n') {
+        if(ch == )
+    }
+
+    
+    return "";
+}
+
+int main()
+{
+    cout << getch();
+}
